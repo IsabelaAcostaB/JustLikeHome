23313e209082eb05da70f5fef9343cba934e4f5a
Gina <ginarm12@hotmail.com>
2022-08-05 12:06:47 -0500

    cleaning

--- BookingProject/src/main/java/com/example/BookingProject/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookingProject.repository;
-
-import com.example.BookingProject.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByTitle(String title);
-}
