ffd15f2a581b29c48bc0981104b9eb50e4271e91
unknown <pilarlb99@gmail.com>
2022-08-08 17:30:49 -0300

    primer intento de implementar el bloque listado

--- Front/digital-booking/src/components/List/List.jsx
+++ Front/digital-booking/src/components/List/List.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import listado from "../listado.json";
 import Header from './Header';
-import Footer from "./Footer"
+import Footer from "./Footer";
+import "bootsrap";
 
+function listado(){
+<div className="card text-center">
+    <img src="../listado.json/listado/img"/>
+    <div className="card-body">
+        <p className="card-text-secondary" src="../listado.json/listado/descripcion"></p>
+         <a href="#!">Ver detalle</a>
+         </div>
+</div>
+}
+export default listado
