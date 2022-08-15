2f3c4f5f98f9e753d2357a3e5be5f98e978dbaf9
Gina <ginarm12@hotmail.com>
2022-08-15 16:44:00 -0500

    S.O.S search bar working but looks ugly

--- Front/digital-booking/src/components/SearchCities/SearchCities.jsx
+++ Front/digital-booking/src/components/SearchCities/SearchCities.jsx
@@ -1,14 +1,32 @@
 import cities from "./co.json"
+import React, { useState } from 'react';
+import { Container } from "react-bootstrap";
 function SearchCities () {
+
+    const[cityId, setCityId]=useState('');
+    
+
+    
+  const handleCity=(e)=>{
+    const getCityId= e.target.value;
+    setCityId(getCityId);
+  }
+
     return (
-        <select>
-            {cities && cities.map(item => (
-                <option key = {item.id} value = {item.city}>
-                    {item.city}
-                </option>
-            ))}
-        </select>
-    )
+    <div>           
+            <select name='city'  onChange={(e)=>handleCity(e)}>
+            <option value="">--Selecciona una ciudad--</option>
+            {
+            cities.map( (getCity,index)=>(
+                <option value={getCity.id} key={index}>{getCity.admin_name}</option> 
+            ))
+            } 
+            </select>           
+        </div>
+        
+        
+        
+)  
 }
 
 export default SearchCities;
