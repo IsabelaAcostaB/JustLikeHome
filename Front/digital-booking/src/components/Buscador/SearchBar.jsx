6b6e7325426afb8e2e4d751545ec2cfd585a602f
zxxej <zxejimenez@gmail.com>
2022-08-15 19:59:55 -0300

    Uniendo

---
+++ Front/digital-booking/src/components/Buscador/SearchBar.jsx
@@ -0,0 +1,73 @@
+import React, { useContext, useState } from "react"
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from "react-router-dom"
+import UserContext from "../context"
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css'; 
+import { DateRangePicker } from 'react-date-range';
+import SearchCities from "../SearchCities/SearchCities";
+import "./searchbar.css"
+
+function SearchBar() {
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
+
+    const resetInput = () => {
+        setSearchInput("");
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    const user = useContext(UserContext)
+    
+
+return (
+
+    <div className="searchbar-container">
+    <h1> Busca ofertas en hoteles, casas y mucho más</h1>
+
+    <div className="search-bar" className="header_center">
+        <SearchCities></SearchCities>
+            {/* <SearchCities/> */}
+            <div className="search-input">
+                <input
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    type="text"placeholder="Inicia tu búsqueda"
+                />
+                
+                <SearchIcon></SearchIcon>
+            </div> 
+            </div>
+
+            {searchInput && 
+                <div>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate = {new Date()}
+                        rangeColors={["#E48561"]}
+                        onChange={handleSelect}
+                    />
+                     <div className="flex">
+                        <button onClick={resetInput} className="flex-grow">Cancelar</button>
+                        <button className="flex-grow">Buscar</button>
+                    </div>
+                
+                </div>  
+                }
+
+            </div> 
+)
+}
+
+export default SearchBar;
