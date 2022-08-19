19182e3a361a0121a922b3516043febdc6f7bee7
aylenvl <aylenvl.16@gmail.com>
2022-08-19 15:56:47 -0300

    archivos sin problemas

--- Front/digital-booking/src/pages/Home/Home.jsx
+++ Front/digital-booking/src/pages/Home/Home.jsx
@@ -1,30 +1,41 @@
 import React from "react";
+
 import ListarCat from "../../components/categories/Categories";
+import axios from 'axios';
+
 import List from "../../components/List/Lists";
+
 import SearchBar from "../../components/Buscador/SearchBar";
 
 
-
 const Home = ()=>{
-
-    return(
-        <div className="main">
-          <SearchBar></SearchBar>
-          <h1 className="category-title">Bienvenido a Just like Home</h1>
-            
-          <h2 className="category-title">Selecciona un tipo de alojamiento</h2>
-          <ListarCat></ListarCat>
-          <h2 className="recommendation-h2">Recomendados</h2>
-
-          <List />
-
-          <ul>
-            
+    const [data, setData] = useState({ List: [] });  
+    useEffect(async () => { 
+        const result = await axios.get(
+          'http://localhost:8080/api/',
+           );
+    
+        setData({List: result.data});
+      }, [] ); 
      
-          </ul>
+    return(
+      <div className="main">
+        <SearchBar></SearchBar>
+        <h1 className="category-title">Bienvenido a Just like Home</h1>
+            
+        <h2 className="category-title">Selecciona un tipo de alojamiento</h2>
+        <ListarCat/>
+        <h2 className="recommendation-h2">Recomendados</h2>
+        <ul>
+            
+          {data && data.List.map(item => (
+            <li key={item.ID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
 
-
-        </div>
+      </div>
     )
 }
 
