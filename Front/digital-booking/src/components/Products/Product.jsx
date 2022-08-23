3a2b49930859bf3af6e0fc9cd0243a6c4eff6c63
zxxej <zxejimenez@gmail.com>
2022-08-23 18:04:25 -0300

    Avances en pagina de producto, fetch con id

--- Front/digital-booking/src/components/Products/Product.jsx
+++ Front/digital-booking/src/components/Products/Product.jsx
@@ -6,167 +6,88 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faShareSquare } from "@fortawesome/free-regular-svg-icons";
 import { faMap } from "@fortawesome/free-regular-svg-icons";
 import axios from "axios";
-import product from "./products.json";
 import ProductHeader from "./ProductHeader";
-import { render } from "@testing-library/react";
 /* import Carousel from "react-multi-carousel"; */
 
-function Renderizar({productInfo}){
-  return(<div className="main">
-  <ProductHeader
-    category={productInfo.category.title}
-    title={productInfo.title}
-  />
-  <div className="product">
-    <div className="product-location-container">
-      <div className="product-location">
-        <p>
-          {productInfo.city.name}, {productInfo.city.name}
-        </p>
-        <p>
-          <FontAwesomeIcon icon={faMap} /> A 320m del centro
-        </p>
-      </div>
-      <div className="product-rating">
-        <div>
-          <p>Muy bueno</p>
-          <FontAwesomeIcon icon={faHeart} />
-          <FontAwesomeIcon icon={faHeart} />
-          <FontAwesomeIcon icon={faHeart} />
-          <FontAwesomeIcon icon={faHeart} />
-          <FontAwesomeIcon icon={faHeart} />
-        </div>
-        <p>8</p>
-      </div>
-    </div>
-    <div>
-      <FontAwesomeIcon icon={faHeart} />
-      <FontAwesomeIcon icon={faShareSquare} />
-    </div>
-    <div className="gallery-container">
-      <img src={productInfo.images.main} alt="" />
-      <div className="mini-gallery">
-        <img src={productInfo.images.other1} alt="" />
-        <img src={productInfo.images.other2} alt="" />
-        <img src={productInfo.images.other3} alt="" />
-        <img src={productInfo.images.other4} alt="" />
-      </div>
-    </div>
-    <div className="description">
-      <p>{productInfo.description}</p>
-    </div>
-  </div>
-</div>)
-}
 
-
-
-function Product(/* { productId } */) {
-  /* const [productInfo, setProductInfo] = useState(product); */
+function Product() {
   const [productInfo, setProductInfo] = useState();
+ 
+  let location = window.location.pathname;
 
   /* ------- CODIGO PARA EL FETCH DEL PRODUCTO ------- */
   function Fetch() {
-    let url = "http://18.216.199.175:8080/api/product/11" /* + productId */;
+    let url = "http://18.216.199.175:8080/api" + location;
     useEffect(() => {
       axios.get(url)
         .then((response) => setProductInfo(response.data))
-        /* .then((response) => console.log(response)) */
         .catch((error) => console.log(error));
     }, [url]);
   }
 
   Fetch();
-  /* console.log(productInfo) */
-  /* function carouselGallery() {
-    <Carousel
-      showArrows={true}
-      onChange={onChange}
-      onClickItem={onClickItem}
-      onClickThumb={onClickThumb}
-    >
-      <div>
-        <img src="assets/1.jpeg" />
-        <p className="legend">Legend 1</p>
-      </div>
-      <div>
-        <img src="assets/2.jpeg" />
-        <p className="legend">Legend 2</p>
-      </div>
-      <div>
-        <img src="assets/3.jpeg" />
-        <p className="legend">Legend 3</p>
-      </div>
-      <div>
-        <img src="assets/4.jpeg" />
-        <p className="legend">Legend 4</p>
-      </div>
-      <div>
-        <img src="assets/5.jpeg" />
-        <p className="legend">Legend 5</p>
-      </div>
-      <div>
-        <img src="assets/6.jpeg" />
-        <p className="legend">Legend 6</p>
-      </div>
-    </Carousel>;
-  } */
 
-/*   render(Renderizar({productInfo})) */
   return(
-    console.log(productInfo)
-    /* DE ACA PARA ABAJO SE COMENTA TODO Y FUNCIONA EL FETCH */
-   /*  ----------------------------------------------------- */
-    /* <Renderizar productInfo={productInfo}/> */
     
-    /* console.log(productInfo) */
-    ,
     <div className="main">
-      <ProductHeader
-        category={productInfo.category.title}
-        title={productInfo.title}
-      />
-      <div className="product">
-        <div className="product-location-container">
-          <div className="product-location">
-            <p>
-              {productInfo.city.name}, {productInfo.city.name}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faMap} /> A 320m del centro
-            </p>
-          </div>
-          <div className="product-rating">
-            <div>
-              <p>Muy bueno</p>
-              <FontAwesomeIcon icon={faHeart} />
-              <FontAwesomeIcon icon={faHeart} />
-              <FontAwesomeIcon icon={faHeart} />
-              <FontAwesomeIcon icon={faHeart} />
-              <FontAwesomeIcon icon={faHeart} />
+      {productInfo && <div className="main">
+        <ProductHeader
+          category={productInfo.category.title}
+          title={productInfo.title}
+        />
+        <div className="product">
+          <div className="product-location-container">
+
+            <div className="product-location">
+              <p>
+                {productInfo.city.name}, {productInfo.city.country}
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faMap} /> A 320m del centro
+              </p>
             </div>
-            <p>8</p>
+
+            <div className="product-rating">
+              <div>
+                <p>Muy bueno</p>
+                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon icon={faHeart} />
+              </div>
+              <p>8</p>
+            </div>
           </div>
-        </div>
-        <div>
-          <FontAwesomeIcon icon={faHeart} />
-          <FontAwesomeIcon icon={faShareSquare} />
-        </div>
-        <div className="gallery-container">
-          <img src={productInfo.images.main} alt="" />
-          <div className="mini-gallery">
-            <img src={productInfo.images.other1} alt="" />
-            <img src={productInfo.images.other2} alt="" />
-            <img src={productInfo.images.other3} alt="" />
-            <img src={productInfo.images.other4} alt="" />
+
+
+          <div className="socialIcons">
+            <FontAwesomeIcon icon={faHeart} />
+            <FontAwesomeIcon icon={faShareSquare} />
           </div>
-        </div>
-        <div className="description">
-          <p>{productInfo.description}</p>
+
+          <div className="gallery-container">
+            <img src={productInfo.images.main} alt="" />
+            <div className="mini-gallery">
+              <img src={productInfo.images.other1} alt="" />
+              <img src={productInfo.images.other2} alt="" />
+              <img src={productInfo.images.other3} alt="" />
+              <img src={productInfo.images.other4} alt="" />
+            </div>
+          </div>
+          <div className="description">
+            <h2>{productInfo.description_title}</h2>
+            <p>{productInfo.description}</p>
+          </div>
+          <div className="amenities">
+
+          </div>
         </div>
       </div>
+    } 
     </div>
-  );
+
+  )
 }
 
 export default Product;
