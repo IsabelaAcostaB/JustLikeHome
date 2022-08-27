f43e7745088803f233c9170c950fb852eb010d1f
Gina <ginarm12@hotmail.com>
2022-08-26 23:15:53 -0500

    swagger working on localhost

--- BackEnd/PI/src/main/java/com/example/BookingProject/bookingAPI/config/WebConfig.java
+++ BackEnd/PI/src/main/java/com/example/BookingProject/bookingAPI/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.BookingProject.bookingAPI.controller;
+package com.example.BookingProject.bookingAPI.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
