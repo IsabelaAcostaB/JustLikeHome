a1a65be38b4082066d0290250b5960e7dd348e90
Gina <ginarm12@hotmail.com>
2022-08-29 14:53:56 -0500

    signup-signin error

---
+++ BackEnd/PI/src/main/java/com/example/BookingProject/bookingAPI/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.BookingProject.bookingAPI.controller;
+
+import com.example.BookingProject.bookingAPI.persistence.model.Role;
+import com.example.BookingProject.bookingAPI.security.UserPrinciple;
+import com.example.BookingProject.bookingAPI.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("change/{role}")
+    public ResponseEntity<?> changeRole(@AuthenticationPrincipal UserPrinciple userPrinciple, @PathVariable Role role) {
+        userService.changeRole(role, userPrinciple.getUsername());
+        return ResponseEntity.ok(true);
+    }
+}
