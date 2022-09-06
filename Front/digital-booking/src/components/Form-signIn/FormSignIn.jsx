ac9912774f78173d45ca57302da147cc814d71b8
zxxej <zxejimenez@gmail.com>
2022-09-06 01:54:55 -0300

    Error en merge

--- Front/digital-booking/src/components/Form-signIn/FormSignIn.jsx
+++ Front/digital-booking/src/components/Form-signIn/FormSignIn.jsx
@@ -1,53 +1,35 @@
-import formData from "../../db/formData";
-import React, { useState, useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import formSignIn from "./formSignIn.css";
+import formData from "../../db/formData"
+import React, { useState, useContext, useEffect  } from "react";
+import {Link} from "react-router-dom"
+import formSignIn from "./formSignIn.css"
 import { UserContext } from "../UserContext.jsx";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import Url from "../../util/Url";
+import {useNavigate} from "react-router-dom"
+import axios from "axios"
 
-const FormSignIn = () => {
-  const { userData, setUserData } = useContext(UserContext);
+const FormSignIn = () =>{
 
-  const [validData, setValidData] = useState(true);
+    const {userData, setUserData} = useContext(UserContext)
 
-  const navigate = useNavigate();
+    const [validData, setValidData] = useState(true);
 
-  useEffect(() => {
-    const postData = async () => {
-      const url = Url() + "/api/authentication/sign-in";
-      const result = await axios.post(url, userData);
+    const navigate = useNavigate();
 
-      console.log(result);
+    const getValues = (target) =>{
+        return {
+            email: target.email.value,
+            password: target.password.value,
+        }
+    }
 
-      /*
-            if(result.status === 200){
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
 
-               
-                const userDataLog = {
-                    name: userData.name,
-                    lastName: userData.lastName,
-                    isLogged: true
-                }
-                setUserData(userDataLog);
-               
-                navigate("/")
-                
-            }
-            */
-    };
-  });
+        const userDataForm = getValues(event.target);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const userDataForm = getValues(event.target);
-
-    const url = "http://18.217.103.69:8080/api/authentication/sign-in";
-    const result = await axios.post(url, userDataForm);
-
-    /* 
+        const url = "http://18.217.103.69:8080/api/authentication/sign-in";
+        const result = await axios.post(url, userDataForm);
+        
+        /* 
         {
             headers:{
                 'Authorization': `Bearer ${localStorage.getItem('jwt')}`
@@ -56,57 +38,60 @@ const FormSignIn = () => {
 
         */
 
-    if (result.request.status === 200) {
-      const userDataLog = {
-        name: result.data.name,
-        lastName: result.data.lastName,
-        isLogged: true,
-        token: result.data.token,
-      };
 
-      if (result.data.token) {
-        localStorage.setItem("jwt", result.data.token);
-      }
+        if (result.request.status === 200){
+            const userDataLog = {
+                name: result.data.name,
+                lastName: result.data.lastName,
+                isLogged: true,
+                token:result.data.token
+            }
 
-      setUserData(userDataLog);
-      navigate("/");
-    } else {
-      setValidData(false);
+            if (result.data.token){
+                localStorage.setItem('jwt', result.data.token)
+            }
+
+            setUserData(userDataLog);
+            navigate("/")
+            
+        }else{
+            setValidData(false)
+        }
+       
     }
-  };
 
-  return (
-    <div className="main-signIn">
-      <form onSubmit={handleSubmit}>
-        <div className="form-header">
-          <h2>Iniciar sesión</h2>
+    return (
+        <div className="main-signIn">
+            <form onSubmit={handleSubmit}>
+                <div className="form-header">
+                    <h2>Iniciar sesión</h2>
+                </div>
+
+                <div className="form-labels">
+                    <label htmlFor="email">Correo electrónico:</label>
+                    <input type="email"  name="email" id="email"/>
+                    
+                
+                    <label htmlFor="password">Contraseña:</label>
+                    <input type="password" name="password" id="password"/>
+                    
+                </div>
+                <p className ={validData?"hide":"error"}>Por favor vuelva a intentarlo, sus credenciales son inválidas</p>
+        
+
+
+                <button className="button-2" type="submit" >Ingresar</button>
+
+                <div className="go-sign">
+                    <Link to="/signUp">
+                        <p>¿Aún no tiene cuenta? <span>Registrese</span></p>
+                    </Link>
+                </div>
+
+            </form>
+
         </div>
+    )
+}
 
-        <div className="form-labels">
-          <label htmlFor="email">Correo electrónico:</label>
-          <input type="email" name="email" id="email" />
-
-          <label htmlFor="password">Contraseña:</label>
-          <input type="password" name="password" id="password" />
-        </div>
-        <p className={validData ? "hide" : "error"}>
-          Por favor vuelva a intentarlo, sus credenciales son inválidas
-        </p>
-
-        <button className="button-2" type="submit">
-          Ingresar
-        </button>
-
-        <div className="go-sign">
-          <Link to="/signUp">
-            <p>
-              ¿Aún no tiene cuenta? <span>Registrese</span>
-            </p>
-          </Link>
-        </div>
-      </form>
-    </div>
-  );
-};
-
-export default FormSignIn;
+export default FormSignIn
