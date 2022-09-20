5a188c1deeb6350e343d4db9b3c5bb18800b4f01
unknown <pilarlb99@gmail.com>
2022-09-20 16:34:13 -0300

    checkbox ready

--- Front/digital-booking/src/App.js
+++ Front/digital-booking/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Home from "./pages/Home/Home"
 import app from "./styles/App.css"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 import Header from "./components/Header/Index";
 import Footer from "./components/Footer/Index";
 import Listar from "./components/List/List";
 import ProductPage from "./pages/Product/ProductTemplate";
-import {UserProvider} from "./components/UserContext";
-import {FilterProvider} from "./components/FilterContext";
+import { UserProvider } from "./components/UserContext";
+import { FilterProvider } from "./components/FilterContext";
 import Reservation from "./components/Reservation/Reservation"
 import PrivateRoute from "./components/RequireAuth";
 import ConfirmationMessage from "./components/Reservation/ConfirmationMessage";
-import {ReservationProvider} from "./components/ReservationContext"
+import { ReservationProvider } from "./components/ReservationContext"
 import SearchTemplate from "./components/SearchPage/SearchTemplate"
 import Search from "./pages/Search/Search"
 import CreateProducts from "./components/CreateProducts/CreateProducts";
@@ -27,44 +27,45 @@ function App() {
     <div className="App">
 
       <UserProvider>
-      <FilterProvider>
-      <ReservationProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/listar" element={<Listar />}/> 
-          <Route path="/product/:id" element={<ProductPage />}/> 
-          <Route path="/filtrado" element={<Search />}/> 
+        <FilterProvider>
+          <ReservationProvider>
+            <BrowserRouter>
+              <Header />
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/signIn" element={<SignIn />} />
+                <Route path="/listar" element={<Listar />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+                <Route path="/filtrado" element={<Search />} />
 
 
-          
-            <Route path="/administration" element={<CreateProducts />}/> 
-            <Route path="/ConfirmationProduct" element={<ConfirmationMessage message={"Su propiedad fue creada con éxito"}/>}/>
-          
-          
-          
-          {/* 
+
+                
+                <Route path="/ConfirmationProduct" element={<ConfirmationMessage message={"Su propiedad fue creada con éxito"} />} />
+
+
+
+                {/* 
           <Route element={<AdminRoute />}>
           </Route>
           */}
-          
-
-          <Route element={<PrivateRoute />}>
-          <Route path="/product/reservation/:id" element={<Reservation />}/>
-          <Route path="/ConfirmationMessage" element={<ConfirmationMessage message={"Su reservación se ha realizado con éxito"}/>}/> 
-          </Route>
-          <Route path="/administration" element={<CreateProducts />}/>
 
 
-        </Routes>
-        
-        <Footer />
-      </BrowserRouter>
-      </ReservationProvider>
-      </FilterProvider>
+                <Route element={<PrivateRoute />}>
+                  <Route path="/product/reservation/:id" element={<Reservation />} />
+                  <Route path="/ConfirmationMessage" element={<ConfirmationMessage message={"Su reservación se ha realizado con éxito"} />} />
+                  <Route path="/administration" element={<CreateProducts />} />
+
+                </Route>
+                
+
+              </Routes>
+
+              <Footer />
+            </BrowserRouter>
+          </ReservationProvider>
+        </FilterProvider>
       </UserProvider>
 
     </div>
